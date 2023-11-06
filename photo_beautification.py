7d2e4594efee6fd6236a43ce29dbb9d661d08281
林月偉 <140767891+yue-wei-Lin@users.noreply.github.com>
2023-11-06 17:27:21 +0800

    Update photo_beautification.py

--- photo_beautification.py
+++ photo_beautification.py
@@ -28,3 +28,10 @@ cv2.imwrite('output.jpg', adjusted)
 # 顯示原始圖片和處理後的圖片（需要在窗口中顯示圖片時使用）
 cv2_imshow(image)
 cv2_imshow(adjusted)
+
+#在這個程式中，我們首先讀取一張名為 input.jpg 的圖片。然後，我們使用均值模糊（GaussianBlur）來獲得模糊效果，
+#使用卷積核進行銳化效果，最後使用 convertScaleAbs 函數來調整亮度和對比度。處理後的圖片將保存為 output.jpg，
+#同時也會在窗口中顯示原始圖片和處理後的圖片。
+
+#請確保你已經將一張名為 input.jpg 的圖片放在程式所在的目錄下，並確保你已經安裝了 OpenCV。
+#你可以根據需要調整程式中的參數以及使用其他的圖像處理技術來實現不同的美化效果。
